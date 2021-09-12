b422507ce70c80064eabaaf847aa21d69cf94daa
Alexander Voroshilov <pinatree.workmail@gmail.com>
2021-09-12 21:03:20 +0300

    .

--- AutoAnalyticsServer.SqlServerRepository/AutoAnalyticsPostgresDBContext.cs
+++ AutoAnalyticsServer.SqlServerRepository/AutoAnalyticsPostgresDBContext.cs
@@ -31,7 +31,7 @@ namespace AutoAnalytics.WebPortal.DAL.PostgreSQL
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseNpgsql("Host=192.168.0.108;Database=dreamscape_portal_db;Username=postgres;Password=qwe1");
+                //optionsBuilder.UseNpgsql("Host=192.168.0.108;Database=dreamscape_portal_db;Username=postgres;Password=qwe1");
             }
         }
 
